e614d57db129b8f8fba91a894f4f4cc31ecff264
Bob Bonanno <b0bb0nann0@comcast.net>
2017-12-15 13:51:27 -0600

    oops

--- Chocohalics-GitUpload/src/backEnd/ProviderHash.java
+++ Chocohalics-GitUpload/src/backEnd/ProviderHash.java
@@ -1,8 +1,21 @@
 package backEnd;
+import java.io.File;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.HashMap;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class ProviderHash extends DataStoreHash<Provider>{
 
@@ -77,25 +90,87 @@ public class ProviderHash extends DataStoreHash<Provider>{
 
 	@Override
 	public String getDataHashType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	protected Node getXMLElement(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		return "Provider";
 	}
 
 	@Override
 	public void writeToXML() {
-		// TODO Auto-generated method stub
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		
+		Date date = new Date(System.currentTimeMillis());
+		Time time = new Time(System.currentTimeMillis());
+		String hashType = getDataHashType();
+		
+		try{
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.newDocument();
+			//add element
+			Element rootElement = doc.createElement("ChocAn_"+hashType);
+			doc.appendChild(rootElement);
+			
+			
+			for(Integer i : providersHash.keySet()){
+				rootElement.appendChild(providersHash.get(i).getXMLElement(doc));
+			}
+			
+			//for output to file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			//for pretty print
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+			
+			//write to file
+			@SuppressWarnings("deprecation")
+			StreamResult file = new StreamResult(new File("ChocAn"+hashType+"_"+date.toString()+"_"+time.getHours()+"-"+time.getMinutes()+".XML"));
+			
+			transformer.transform(source, file);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void readFromXML(String FileName) {
-		// TODO Auto-generated method stub
+		try {
+			File file = new File(FileName);
+			
+			DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			
+			Document doc = dBuilder.parse(file);
+			
+			doc.getDocumentElement().normalize();
+		
+			NodeList nList = doc.getElementsByTagName("Service");
+				
+			for(int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				
+				//for debugging
+				System.out.println("Current element"+nNode.getNodeName());
+				
+				if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					Provider provider = new Provider();
+					
+					provider.setIDNum(providerID);
+					
+					service.setID(Integer.parseInt(eElement.getAttribute("ServiceID")));
+					service.setName(eElement.getElementsByTagName("ServiceName").item(0).getTextContent());
+					
+					service.setFee(Double.parseDouble(eElement.getElementsByTagName("ServiceFee").item(0).getTextContent()));
+					service.setDescrp(eElement.getElementsByTagName("ServiceDescription").item(0).getTextContent());
+					
+					System.out.println(service.toString());
+					servicesHash.put(service.getID(), service);
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
